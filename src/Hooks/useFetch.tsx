2a0da589bb84f63bedaf183f2869192c4bfa0858
Ian_França <iiann_2009@hotmail.com>
2023-09-26 09:55:18 -0300

    cria o hook useFetch

--- src/Hooks/useFetch.tsx
+++ src/Hooks/useFetch.tsx
@@ -1,5 +1,39 @@
-import React from "react";
+import { useEffect, useRef, useState } from "react";
 
-export const useFetch = () => {
-  return <div>useFetch</div>;
-};
+function useFetch<T>(input: RequestInfo | URL, init?: RequestInit | undefined) {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const optionsRef = useRef(init);
+  optionsRef.current = init;
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+    setLoading(true);
+    fetch(input, { signal, ...optionsRef.current })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject();
+      })
+      .then((json: T) => {
+        if (!signal.aborted) setData(json);
+      })
+      .catch((error) => {
+        if (!signal.aborted && error instanceof Error)
+          console.log(error.message);
+      })
+      .finally(() => {
+        if (!signal.aborted) setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, [input]);
+
+  return { data, loading, error };
+}
+
+export default useFetch;
