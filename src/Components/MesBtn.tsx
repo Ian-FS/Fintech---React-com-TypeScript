9bd60a4ecd476f7d9142b04072696b1368f6071e
Ian_França <iiann_2009@hotmail.com>
2023-10-04 10:42:02 -0300

    estiliza botões do componente MesBtn

--- src/Components/MesBtn.tsx
+++ src/Components/MesBtn.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 
+const style: React.CSSProperties = {
+  padding: "var(--gap) var(--gap-s)",
+  backgroundColor: "var(--color-3)",
+  border: "none",
+  borderRadius: "var(--gap)",
+  color: "var(--color-2)",
+  fontWeight: "600",
+  textTransform: "capitalize",
+};
+
 function nomeMes(n: number) {
   const date = new Date();
   date.setMonth(date.getMonth() + n);
-  console.log(date.getMonth());
   return new Intl.DateTimeFormat("pt-BR", { month: "long" }).format(date);
 }
 
 const MesBtn = ({ n }: { n: number }) => {
   nomeMes(n);
-  return <div>{nomeMes(n)}</div>;
+  return <button style={style}>{nomeMes(n)}</button>;
 };
 
 export default MesBtn;
