2e27fceba6918b1232c1416f66c66a20f8c816da
Ian_França <iiann_2009@hotmail.com>
2023-10-04 11:32:17 -0300

    altera exibição em Resumo

--- src/Pages/Resumo.tsx
+++ src/Pages/Resumo.tsx
@@ -3,11 +3,6 @@ import { useData } from "../Context/DataContext";
 
 export const Resumo = () => {
   const { data } = useData();
-  return (
-    <ul>
-      {data?.map((venda) => (
-        <li key={venda.id}>{venda.nome}</li>
-      ))}
-    </ul>
-  );
+  console.log(data);
+  return;
 };
