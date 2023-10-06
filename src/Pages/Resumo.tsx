14d64a772bbd8787de247f348dd8e0477a9bc6a1
Ian_França <iiann_2009@hotmail.com>
2023-10-06 11:27:30 -0300

    mostra ao usuario no componente Resumo: vendas totais, pagas e processando

--- src/Pages/Resumo.tsx
+++ src/Pages/Resumo.tsx
@@ -3,6 +3,48 @@ import { useData } from "../Context/DataContext";
 
 export const Resumo = () => {
   const { data } = useData();
-  console.log(data);
-  return;
+  if (data === null) return null;
+
+  return (
+    <section>
+      <div className="resumo flex mb">
+        <div className="box">
+          <h2>Vendas</h2>
+          <span>
+            {data
+              .filter((item) => item.status !== "falha")
+              .reduce((acc, iten) => acc + iten.preco, 0)
+              .toLocaleString("pt-br", {
+                style: "currency",
+                currency: "BRL",
+              })}
+          </span>
+        </div>
+        <div className="box">
+          <h2>Recebido</h2>
+          <span>
+            {data
+              .filter((item) => item.status === "pago")
+              .reduce((acc, iten) => acc + iten.preco, 0)
+              .toLocaleString("pt-br", {
+                style: "currency",
+                currency: "BRL",
+              })}
+          </span>
+        </div>
+        <div className="box">
+          <h2>Processando</h2>
+          <span>
+            {data
+              .filter((item) => item.status === "processando")
+              .reduce((acc, iten) => acc + iten.preco, 0)
+              .toLocaleString("pt-br", {
+                style: "currency",
+                currency: "BRL",
+              })}
+          </span>
+        </div>
+      </div>
+    </section>
+  );
 };
