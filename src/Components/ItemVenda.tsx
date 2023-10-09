3b2aed06b46eb12100def1016e40121e106ba209
Ian_França <iiann_2009@hotmail.com>
2023-10-09 03:30:42 -0300

    cria lógica de exibição de dados de venda no componente ItemVenda

--- src/Components/ItemVenda.tsx
+++ src/Components/ItemVenda.tsx
@@ -1,7 +1,20 @@
 import { VendasType } from "../Context/DataContext";
 
 const ItemVenda = ({ venda }: { venda: VendasType }) => {
-  return <div className="venda box">ItemVenda</div>;
+  return (
+    <div className="venda box">
+      <a href="" style={{ fontFamily: "monospace" }}>
+        {venda.id}
+      </a>
+      <div>{venda.nome}</div>
+      <div>
+        {venda.preco.toLocaleString("pt-br", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default ItemVenda;
