448dced3019c3b798332018dd1c942c2406dc7ca
Ian_França <iiann_2009@hotmail.com>
2023-10-14 11:15:23 -0300

    faz com que o cabaçalho mude ao mudar de página

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DateRange from "./DateRange";
 import Meses from "./Meses";
+import { useLocation } from "react-router-dom";
 
 export const Header = () => {
   const [title, setTitle] = useState("Resumo");
+
+  const { pathname } = useLocation();
+  useEffect(() => {
+    console.log(pathname);
+    switch (pathname) {
+      case "/":
+        setTitle("Resumo");
+        document.title = "Fintech | Resumo";
+        break;
+      case "/vendas":
+        setTitle("Vendas");
+        document.title = "Fintech | Vendas";
+        break;
+
+      default:
+        break;
+    }
+  }, [pathname]);
 
   return (
     <header className="mb">
