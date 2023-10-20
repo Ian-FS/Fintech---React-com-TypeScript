b4097255f54ef518bcaa0afd9d335f370aa39391
Ian_França <iiann_2009@hotmail.com>
2023-10-20 20:17:26 -0300

    cria função que trata dados

--- src/Components/GraficosVendas.tsx
+++ src/Components/GraficosVendas.tsx
@@ -1,22 +1,37 @@
-import { CartesianGrid, Line, LineChart, Tooltip, XAxis } from "recharts";
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+} from "recharts";
 import { VendasType, useData } from "../Context/DataContext";
 
 const GraficoVendas = ({ data }: { data: VendasType[] }) => {
-  console.log(data[0].data.substring(0, 10));
+  const { inicio, final } = useData();
+  function trataDados() {
+    data.filter((venda) => venda.data >= inicio && venda.data <= final);
+    return data;
+  }
+
+  console.log(trataDados()[0]);
   return (
-    <LineChart
-      width={400}
-      height={400}
-      data={data}
-      margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
-    >
-      <XAxis dataKey="data" />
-      <Tooltip />
-      <CartesianGrid stroke="#f5f5f5" />
-      <Line type="monotone" dataKey="pago" stroke="#ff7300" />
-      <Line type="monotone" dataKey="processando" stroke="#387908" />
-      <Line type="monotone" dataKey="falha" stroke="#387908" />
-    </LineChart>
+    <ResponsiveContainer width="99%" height={400}>
+      <LineChart
+        width={400}
+        height={400}
+        data={trataDados()}
+        margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+      >
+        <XAxis dataKey="data" />
+        <Tooltip />
+        <CartesianGrid stroke="#f5f5f5" />
+        <Line type="monotone" dataKey="status" stroke="#ff7300" />
+        <Line type="monotone" dataKey="pagamento" stroke="#387908" />
+        <Line type="monotone" dataKey="parcelas" stroke="#100879" />
+      </LineChart>
+    </ResponsiveContainer>
   );
 };
 
